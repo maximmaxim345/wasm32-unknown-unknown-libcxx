9992e1ece3bca6e2a5172abe14263a5123bbf4c5
Maxim Raznatovski <nda.mr43@gmail.com>
2024-11-12 18:21:31 +0100

    fix(example): use C++14 for compiling
    Fixes building issues on macos

--- example/build.rs
+++ example/build.rs
@@ -10,13 +10,16 @@ fn main() {
     println!("cargo:rerun-if-changed=test.hpp");
 
     let cxx_flags = std::env::var("CXXFLAGS").unwrap_or_default();
-    let all_clang_args: Vec<&str> = cxx_flags.split_whitespace().collect();
-    let autocxx_build =
+    let all_clang_args: Vec<&str> = cxx_flags
+        .split_whitespace()
+        .chain(std::iter::once("-std=c++14"))
+        .collect();
+    let mut autocxx_build =
         autocxx_build::Builder::new("src/autoffi.rs", [&std::path::PathBuf::from(".")])
             .extra_clang_args(&all_clang_args)
             .build()
             .unwrap();
 
-    autocxx_build.compile("occara-autocxx-bridge");
+    autocxx_build.std("c++14").compile("occara-autocxx-bridge");
     println!("cargo:rerun-if-changed=src/autoffi.rs");
 }
