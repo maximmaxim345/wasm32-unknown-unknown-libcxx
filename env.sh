4e668558589057e19d2707d5e9a509385cb5b871
Maxim Raznatovski <nda.mr43@gmail.com>
2024-11-17 12:15:22 +0100

    refactor: remove use of unnecessary SCRIPT_DIR env var

--- env.sh
+++ env.sh
@@ -3,16 +3,15 @@
 # Get script directory in a shell-agnostic way
 if [ -n "${BASH_SOURCE[0]}" ]; then
     # bash
-    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    WASM32_UNKNOWN_UNKNOWN_STDLIB="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 elif [ -n "$ZSH_VERSION" ]; then
     # zsh
-    SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+    WASM32_UNKNOWN_UNKNOWN_STDLIB="$(cd "$(dirname "$0")" && pwd)"
 else
     # fallback for sh
-    SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+    WASM32_UNKNOWN_UNKNOWN_STDLIB="$(cd "$(dirname "$0")" && pwd)"
 fi
 
-export WASM32_UNKNOWN_UNKNOWN_STDLIB=$SCRIPT_DIR
 export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUSTFLAGS="-C link-arg=-L$WASM32_UNKNOWN_UNKNOWN_STDLIB/lib/ -L $WASM32_UNKNOWN_UNKNOWN_STDLIB/lib/ -lstatic=c -lstatic=c++abi --Z wasm_c_abi=spec"
 export CFLAGS_wasm32_unknown_unknown="--sysroot=$WASM32_UNKNOWN_UNKNOWN_STDLIB -D__OpenBSD__ -D__WASM32_UNKNOWN_UNKNOWN__"
 export CXXFLAGS_wasm32_unknown_unknown="$CFLAGS_wasm32_unknown_unknown -fexceptions"
