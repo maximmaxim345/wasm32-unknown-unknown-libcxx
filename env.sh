48e3e4e504388f83982464aa97c039b4be2ec230
Maxim Raznatovski <nda.mr43@gmail.com>
2024-11-17 12:16:46 +0100

    fix: use of env.sh on sh
    In case of `sh`, we now just use the current dir or the manually set
    `WASM32_UNKNOWN_UNKNOWN_STDLIB_DIR` env var as the directory of libcxx.

--- env.sh
+++ env.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 # Get script directory in a shell-agnostic way
-if [ -n "${BASH_SOURCE[0]}" ]; then
+if [ -n "${BASH_SOURCE}" ]; then
     # bash
     WASM32_UNKNOWN_UNKNOWN_STDLIB="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 elif [ -n "$ZSH_VERSION" ]; then
     # zsh
     WASM32_UNKNOWN_UNKNOWN_STDLIB="$(cd "$(dirname "$0")" && pwd)"
 else
-    # fallback for sh
-    WASM32_UNKNOWN_UNKNOWN_STDLIB="$(cd "$(dirname "$0")" && pwd)"
+    # fallback for sh, we can't determine this scripts directory, so we assume the current directory
+    # if it's not, you can set WASM32_UNKNOWN_UNKNOWN_STDLIB_DIR to the correct directory
+    if [ -n "$WASM32_UNKNOWN_UNKNOWN_STDLIB_DIR" ]; then
+        WASM32_UNKNOWN_UNKNOWN_STDLIB="$WASM32_UNKNOWN_UNKNOWN_STDLIB_DIR"
+    else
+        WASM32_UNKNOWN_UNKNOWN_STDLIB="$(pwd)"
+    fi
 fi
 
 export CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUSTFLAGS="-C link-arg=-L$WASM32_UNKNOWN_UNKNOWN_STDLIB/lib/ -L $WASM32_UNKNOWN_UNKNOWN_STDLIB/lib/ -lstatic=c -lstatic=c++abi --Z wasm_c_abi=spec"
